d079d342966a8d5387bc9175a8470adfaa58eb81
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-03-03 13:29:41 +0600

    Редактирование формы, разделение модели

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 from django.forms import widgets
+from django.core.exceptions import ValidationError
+from webapp.models import Task
 
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Заголовок')
-    description = forms.CharField(
-        max_length=3000, required=True, label='Описание', widget=widgets.Textarea)
-    status = forms.CharField(max_length=200, required=True, label='Статус')
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = {'title', 'description', 'status', 'completion_date'}
+        labels = {
+            'title': 'Заголовок',
+            'description': 'Описание',
+            'status': 'Статус',
+            'completion_date': 'Выполнить до"'
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 2:
+            raise ValidationError('Заголовок должен быть длиннее 2 символов')
+        return title
---
+++ source/webapp/models/tasks.py
@@ -0,0 +1,51 @@
+from django.db import models
+from static.classes.static import Static
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        verbose_name="Заголовок"
+        )
+    description = models.TextField(
+        max_length=3000,
+        null=False,
+        blank=False,
+        verbose_name="Описание"
+        )
+    status = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        choices=Static.choices,
+        default='new',
+        verbose_name="Статус"
+        )
+    is_deleted = models.BooleanField(
+        verbose_name='удалено',
+        null=False,
+        default=False
+        )
+    completion_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Выполнить до"
+        )
+    deleted_at = models.DateField(
+        null=True,
+        default=None,
+        verbose_name="Дата удаления"
+        )
+
+    def __str__(self):
+        return f"{self.title} - {self.description}"
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
